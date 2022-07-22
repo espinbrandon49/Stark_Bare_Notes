2ed06125cab0c2774d90f0edcb3f956d32948ef4
Brandon Espinosa <espinbrandon49@gmail.com>
2022-07-21 20:59:22 -0700

    create HTML GET route  return the `notes.html` file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const uuid = require('./helpers/uuid')
 
 const PORT = 3001
 
@@ -15,10 +16,15 @@ app.use(express.urlencoded({ extended: true }))
 // access everything in the 'public folder'
 app.use(express.static('public'))
 
-// access/transfer? the index file to the internet (through PORT?)
+// create HTML GET route to return index.html file from public
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 )
+
+// create HTML GET route  return the `notes.html` file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 
 
