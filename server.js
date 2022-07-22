87515c7a5f718b364dcdc1a03f3a595acd018eec
Brandon Espinosa <espinbrandon49@gmail.com>
2022-07-21 21:38:11 -0700

    GET /api/notes` should read the `db.json` file and return all saved notes as JSON

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+let notes = require('./db/db.json')
 const uuid = require('./helpers/uuid')
 
 const PORT = 3001
@@ -26,8 +27,17 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+// GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+  // send a message to the client - 1,2,3 are all serving the same purpose in different mediums?
+    //msg 1: notes as a console.log in terminal
+  console.log(notes)
 
+    //msg 2 as a json format for the index.js
+  res.json(notes)
 
+  console.info(`${req.method} request received to get reviews`)
+})
 
 
 
