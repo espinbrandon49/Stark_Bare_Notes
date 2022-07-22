d5f745de3627b7de2e13e0b44c243fdf25859200
Brandon Espinosa <espinbrandon49@gmail.com>
2022-07-21 20:25:58 -0700

    create and equip server.js to listen at PORT 3001

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const PORT = 3001
+
+// npm package is a web framework for node
+const app = express()
+
+// server equipped to read json format
+app.use(express.json())
+// server equipped to read urls
+app.use(express.urlencoded({ extended: true }))
+
+// access everything in the 'public folder'
+app.use(express.static('public'))
+
+// access/transfer? the index file to the internet (through PORT?)
+app.get('/', (req, res) => 
+  res.send
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Binds and listens for connections on PORT 3001
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+)
